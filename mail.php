896b415c77a40318652bd045e145d036c7a1773b
Arunit2001 <arunit2001singh@gmail.com>
2021-06-28 12:26:49 +0530

    contact form check

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "arunit2001singh@gmail.com";
+$subject = "Mail From sishubharti";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@sishuBharti.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
